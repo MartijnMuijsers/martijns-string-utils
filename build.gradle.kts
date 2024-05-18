f4835bb79cb011f3d921005cb37391eb964733b7
Martijn Muijsers <martijnmuijsers@live.nl>
2024-05-18 11:00:02 +0200

    Add lint, refactor and license header

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,39 @@
+import java.time.Year
+
 plugins {
     `java-library`
     `maven-publish`
+    alias(libs.plugins.ktlint)
     alias(libs.plugins.lombok)
+    alias(libs.plugins.spotless)
 }
 
 repositories {
     mavenLocal()
     mavenCentral()
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
+    maven("https://repo.maven.apache.org/maven2/")
 }
 
 group = "nl.martijnmuijsers"
-version = "1.0"
+version = "1.0.1"
 description = "Martijn's string utilities"
 
 publishing {
     publications.create<MavenPublication>("maven") {
         from(components["java"])
+    }
+}
+
+val author = "Martijn Muijsers (martijnmuijsers@live.nl)"
+val creationYear = "2018"
+val license = "Licensed under AGPLv3."
+
+spotless {
+    java {
+        importOrder()
+        removeUnusedImports()
+        cleanthat()
+        palantirJavaFormat()
+        licenseHeader("/*\n * © $author $creationYear-${Year.now()}.\n * $license\n */")
     }
 }
---
+++ src/main/java/nl/martijnmuijsers/stringutils/UsefulCharsets.java
@@ -0,0 +1,13 @@
+/*
+ * © Martijn Muijsers (martijnmuijsers@live.nl) 2018-2024.
+ * Licensed under AGPLv3.
+ */
+package nl.martijnmuijsers.stringutils;
+
+import java.nio.charset.Charset;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UsefulCharsets {
+    public static Charset GB18030 = java.nio.charset.Charset.forName("GB18030");
+}
